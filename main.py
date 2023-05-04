8ab4d600534e5c766b1c0d8b7b5d06e42cc0d9ae
Cori Diaz <corinicolediaz@gmail.com>
2023-05-04 01:23:23 -0500

    fully parses and added game functionality

--- main.py
+++ main.py
@@ -1,57 +1,43 @@
-from lark import Lark, Transformer
+from lark import Lark
 from textwrap import dedent
 from random import randint
 
 grammar = ''' 
-    start: ((cast | action | inventory_action | open_inventory) [punctuation])* | exit [punctuation]
+    start: ((cast | action | inventory_action | open_inventory | book) [PUNCTUATION])* | exit [PUNCTUATION]
 
-    cast: "cast" spell [preposition] ["the" | "a"] object 
+    cast: "cast" SPELL [PREPOSITION] ["the" | "a"] OBJECT 
 
-    action: verb [preposition] ["the" | "a"] object 
+    action: VERB [PREPOSITION] ["the" | "a"] OBJECT 
 
-    inventory_action: use | add | "read spellbook" 
+    inventory_action: use | add  | drop
 
     open_inventory: "open inventory" | "open backpack"
 
-    exit: "end game" | "exit game" | "escape"
+    exit: "end game" | "exit game" | "escape" | "exit"
 
-    use: "use" item [preposition object]
+    use: "use" ["the"] ITEM [PREPOSITION OBJECT]
 
-    add: "pickup" item
+    add: ("pickup" | "add") ["the"] ITEM 
 
-    punctuation: "." | "!" | "?" | ","
+    drop: ("drop" | "remove" | "get rid of") ["the"] ITEM
 
-    spell:  "fire" | "water" | "earth" | "air"
+    book: "read spellbook" | "open spellbook"
+
+    PUNCTUATION: "." | "!" | "?" | ","
+
+    SPELL:  "fire" | "water" | "earth" | "air"
          
-    verb: "knock" | "break"
+    VERB: "knock" | "break"
 
-    preposition: "on" | "at" | "in"
+    PREPOSITION: "on" | "at" | "in"
                         
-    object: "sprout"
-            | "door"
-            | "fireplace"
-            | "mist"
+    OBJECT: "sprout" | "door" | "fireplace" | "mist"
 
-    item: "spellbook"
-        | "key"
-        | "lantern"
-        | "potion"
-        | "dagger"
+    ITEM: "key" | "lantern" | "potion" | "dagger" | "spellbook"
 
     %import common.WS 
     %ignore WS 
     '''
-
-
-class GrammarTransformer(Transformer):
-    def get_spell(self, item):
-        # print('here\n')
-        print(item)
-        # return item[0]
-        pass
-    
-    def get_object(self, item):
-        return item[-1]
 
 
 ### GAME GLOBALS ###
@@ -60,76 +46,156 @@ global game
 global inventory
 
 game = True
-inventory = ["spellbook"]
+inventory = ['spellbook', 'key']
 
 
 ### GRAMMAR FUNCTIONS ###
 
 def translate(tree):
-    # transformer = GrammarTransformer()
-    # print(tree)
     if tree.data == 'start':
         for child in tree.children:
             return translate(child)
     elif tree.data == 'cast':
+        return cast_spell(tree.children[0], tree.children[-1])
+    elif tree.data == 'action':
+        return perform_action(tree.children[0], tree.children[-1])
+    elif tree.data == 'inventory_action':
         for child in tree.children:
             return translate(child)
-    elif tree.data == 'spell':
-        
-        # return cast_spell(tree, transformer)
-        return cast_spell(tree, None)
-    elif tree.data == 'action':
-        pass
+    elif tree.data == 'use':
+        return use_item(tree.children[0], tree.children[-1])
+    elif tree.data == 'add':
+        return add_item(tree.children[-1].value)
+    elif tree.data == 'drop':
+        return drop_item(tree.children[-1].value)
     elif tree.data == 'open_inventory':
-        open_inventory()
+        return open_inventory()
+    elif tree.data == 'book':
+        return print_spells()
     elif tree.data == 'exit':
         return end_game()
     else:
         return
 
 
-def cast_spell(tree, transformer):
-    # transformer.get_spell(tree)
-    # print(tree)
-    print()
+def cast_spell(spell, object):
+    if spell == 'fire':
+        if object == 'door':
+            open_door()
+        elif object == 'fireplace':
+            print('\nThe fireplace erupts with a warm glow illuminating the room.\n')
+        elif object == 'mist':
+            random_reply()
+        else:
+            print('\nThe ' + object + ' catches fire but a mysterious force smothers the flame.')
+            print('The ' + object + ' remains unharmed.\n')
+    elif spell == 'water':
+        if object == 'sprout':
+            reveal_door()
+        elif object == 'mist':
+            random_reply()
+        else:
+            print('\nThe ' + object + ' is doused with water but a mysterious force shields the ' 
+                  + object + ' and it remains undisturbed.\n')
+    elif spell == 'earth':
+        if object == 'sprout':
+            reveal_door()
+        elif object == 'mist':
+            random_reply()
+        else:
+            print('\nFor a moment it appears the ' + object + ' is weathered and cracked, and an')
+            print('ancient presence fills the room. In a blink the feeling is gone.') 
+            print('The ' + object + ' appears unphased.\n')
+    elif spell == 'air':
+        if object == 'mist':
+            clear_mist()
+        else:
+            print("\nThe " + object + " receives a gust of wind but mysteriously remains undisturbed.\n")
 
 
-#     if tree.data == 'spell':
-#         for child in tree.children:
-#             return translate_spell(child)
-#     elif tree.data == 'fire':
-#         for child in tree.children:
-#             return translate_spell(child)
-#     elif tree.data == 'water':
-#         pass
-#     elif tree.data == 'earth':
-#         pass
-#     elif tree.data == 'air':
-#         pass
+def perform_action(verb, object):
+    if object == 'door':
+        if verb == 'knock':
+            random_reply()
+        elif verb == 'break':
+            open_door()
+    else:
+        random_reply()
 
 
-
+def use_item(item, object):
+    if item == 'key':
+        if object == 'door':
+            open_door()
+        else:
+            random_reply()
+    else:
+        random_reply()
+    
 
 ### GAME FUNCTIONS ###
 
-def add_object(object):
+def add_item(item):
     global inventory
-    if object not in inventory:
-        inventory.append(object)
-        return inventory
+    if item not in inventory:
+        inventory.append(item)
+        print('\n' + item + " added to your backpack\n")
     else:
-        print(object + " is already in your backpack")
+        print('\n' + item + " is already in your backpack\n")
 
 
-def use_object(object):
-    pass
+def drop_item(item):
+    if item == 'spellbook':
+        print('\nThe book almost hits the ground before it vanishes into thin air...\nand reappears in your backpack.\n')
+    else:
+        global inventory
+        if item in inventory:
+            inventory.remove(item)
+            print('\n' + item + " has been removed from your backpack\n")
+        else:
+            print('\nYou do not have a ' + item + " in your backpack\n")
 
 
 def open_inventory():
     global inventory
-    print("Backpack Inventory:")
+    print("\nBackpack Inventory:")
     for item in inventory:
-        print(item + '\n')
+        print(item)
+    print('\n')
+
+
+def print_spells():
+    print(dedent('''
+    Spells: 
+    fire - creates flame
+    water - creates stream of water
+    earth - grows living things
+    air - creates gust of wind
+    '''))
+
+
+def clear_mist():
+    print('\nThe mist crawls towards the treeline, revealing a single sprout in the center of the clearing.\n')
+
+
+def reveal_door():
+    print(dedent('''
+    The sprout shoots farther and farther out of the earth. It grows limbs that twist in on itself and become gnarled. 
+    In place of the sprout a weathered old tree now stands. 
+    
+    At the base of the tree is a wooden door with a small stained glass window. A dim light glows behind it.  
+    '''))
+
+
+def open_door():
+    print(dedent('''
+    The door swings open to a round room...
+    '''))
+    
+
+def random_reply():
+    responses = ('*crickets*', 'nothing happens.', '*gentle breeze*', '*trees groaning*')
+    print("\n" + responses[randint(0,3)] + "\n")  
 
 
 def end_game():
@@ -140,33 +206,37 @@ def end_game():
 
 def main():
     parser = Lark(grammar)
-    responses = ('*crickets*', 'nothing happens.', '*gentle breeze*')
 
     print(dedent('''
-    Welcome to _____\n
+    Welcome to Wyldlark\n
     You find yourself in the middle of a vast forest clearing.
     All around are towering Oak Trees and mist.\n
-    You carry a backpack containing a battered leather spellbook.
+    You carry a backpack containing a battered leather spellbook and an iron key 
+    with an ornate leaf on its handle.
     '''))
 
-    # while game == True:
-    #     user_input = input('>> ')
-    #     try:
-    #         parse_tree = parser.parse(user_input)
-    #         translate(parse_tree)
-    #     except:
-    #         print("\n" + responses[randint(0,2)] + "\n")
+    while game == True:
+        user_input = input('>> ')
+        try:
+            parse_tree = parser.parse(user_input)
+            translate(parse_tree)
+        except:
+            random_reply()
 
 
     ### TESTING ### 
 
-    user_input = 'open backpack'
-    try:
-        parse_tree = parser.parse(user_input)
-        translate(parse_tree)
-    except:
-        print("\n" + responses[randint(0,2)] + "\n")    
+    # user_input = 'cast earth at fireplace'
+    # user_input = 'read spellbook'
+    # user_input = "pickup dagger"
 
+    # try:
+    #     parse_tree = parser.parse(user_input)
+    #     translate(parse_tree)
+    # except:
+    #     random_reply()  
+
+    # print(parse_tree.pretty())
 
 if __name__ == '__main__':
     main()
